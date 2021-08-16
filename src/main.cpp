ee82f0b8893d58737f3f92f007751377feb70106
MilkJug <76970674+MilkJug1@users.noreply.github.com>
2021-08-16 10:03:17 -0400

    Added the main function

--- src/main.cpp
+++ src/main.cpp
@@ -21,29 +21,45 @@ TO COMPILE USE
 #define ROWS 40
 
 
-
-void init()
-{
-    initGrid(COLUMUS, ROWS);
-}
-
-
 int main()
 {
-    sf::Window* window(sf::VideoMode(800, 600), "Test");
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
 
+    // define the level with an array of tile indices
+    const int level[] =
+    {
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
+
+    // create the tilemap from the level definition
+    TileMap map;
+    if (!map.load("tileset.png", sf::Vector2u(32, 32), level, 16, 8))
+        return -1;
+
+    // run the main loop
     while (window.isOpen())
     {
+        // handle events
         sf::Event event;
         while (window.pollEvent(event))
         {
             if(event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
         }
-    }
 
+        // draw the map
+        window.clear();
+        window.draw(map);
+        window.display();
+    }
 
     return 0;
 }
