c01ad424eb5c820ebdcd7c27102e62fb426cdd14
MilkJug <76970674+waterbottle-123@users.noreply.github.com>
2021-08-18 21:16:14 -0400

    fixed an issue

--- src/grid.hpp
+++ src/grid.hpp
@@ -1,5 +1,5 @@
-#include "SFML-2.5.1/include/SFML/Graphics.hpp"
-#include "SFML-2.5.1/include/SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
 
 class TileMap : public sf::Drawable, public sf::Transformable
 {
